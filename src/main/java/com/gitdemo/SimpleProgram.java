08c8da960c806f99cb8646ffaa84d7ec813295ef
shriniwasalle <shriniwasalle4@gmail.com>
2020-02-08 09:24:04 +0530

    Added sysout 3

--- src/main/java/com/gitdemo/SimpleProgram.java
+++ src/main/java/com/gitdemo/SimpleProgram.java
@@ -4,5 +4,7 @@ public class SimpleProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Hello World1!");
+		
+		System.out.println("Hello World3!");
 	}
 }
