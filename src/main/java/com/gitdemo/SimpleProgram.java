64a83b5e9977483c118c0b3d13aeb7b3657f927b
shriniwasalle <shriniwasalle4@gmail.com>
2020-02-08 09:42:59 +0530

    Created new branch and added sysout

--- src/main/java/com/gitdemo/SimpleProgram.java
+++ src/main/java/com/gitdemo/SimpleProgram.java
@@ -6,5 +6,9 @@ public class SimpleProgram {
 		System.out.println("Hello World1!");
 		
 		System.out.println("Hello World3!");
+
+		System.out.println("Hello World2!");
+		
+		System.out.println("Hello World4");
 	}
 }
